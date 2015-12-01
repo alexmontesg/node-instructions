ca4cddd3ff95bf41f233be0c2fd02a3b07e43801
Alejandro Montes García <alejandro.montes.g@gmail.com>
2015-12-01 10:59:02 +0100

    Allows to change rooms

--- chatrooms/public/javascripts/chat.js
+++ chatrooms/public/javascripts/chat.js
@@ -10,6 +10,12 @@ Chat.prototype.sendMessage = function(room, text) {
 	this.socket.emit('message', message);
 };
 
+Chat.prototype.changeRoom = function(room) {
+	this.socket.emit('join', {
+		newRoom: room
+	});
+};
+
 Chat.prototype.processCommand = function(command) {
 	var words = command.split(' ');
 	var command = words[0].substring(1, words[0].length).toLowerCase();
