48dd2d80f55bbb25d6c08e15fa61daa2a0de9d13
Alejandro Montes García <alejandro.montes.g@gmail.com>
2015-12-01 10:51:18 +0100

    Allows to end the request to the index without the slash

--- chatrooms/server.js
+++ chatrooms/server.js
@@ -38,7 +38,7 @@ function serveStatic(response, cache, absPath) {
 
 var server = http.createServer(function(request, response) {
 	var filePath = false;
-	if(request.url == '/~amontes/') {
+	if(request.url == '/~amontes/' || request.url == '/~amontes') {
 		filePath = 'public/index.html';
 	} else {
 		filePath = 'public' + request.url.replace('~amontes/', '');
